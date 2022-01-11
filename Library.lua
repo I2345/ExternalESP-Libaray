6f94877970473520fb69df6d0ad59db8a884dca5
I2345 <57865481+I2345@users.noreply.github.com>
2022-01-11 17:26:58 +0000

    Create Library.lua

---
+++ Library.lua
@@ -0,0 +1,63 @@
+local activeESP = {
+    ["players"] = {
+
+    };
+
+    ["objects"] = {
+
+    };
+
+    
+}
+
+local Set = {
+    ["ESP"] = {
+        ["TeamCheck"] = false;
+    }
+}
+
+local meta = {}
+
+function meta.Disable(ESPType)
+
+    for i,v in pairs(activeESP) do
+        if i == string.lower(ESPType) then
+            for a,e in pairs(v) do
+                e:Destroy()
+            end
+            v = {}
+        end
+    end
+
+    return
+
+end
+
+function makeNewBoxHandler(Type, obj)
+    local newBox = Instance.new("BoxHandleAdornment")
+    newBox.Adornee = obj
+    newBox.AlwaysOnTop = true
+    newBox.Size = obj.Size
+    newBox.Visible = true
+    newBox.Transparency = 0
+    newBox.ZIndex = 100000
+    table.insert(activeESP[Type], newBox)
+end
+
+function meta.APIEnabled(ESPType, obj)
+
+    --[[
+        Types:
+        - players
+        - objects
+    ]]
+
+    if activeESP[string.lower(ESPType)] then
+
+        makeNewBoxHandler(string.lower(ESPType), obj)
+
+    end
+
+end
+
+return meta
